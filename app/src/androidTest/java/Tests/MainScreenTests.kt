21b1f403cffb55ad0d568897b21ff3f1582a7cef
Nikolay <nmz11@mail.ru>
2026-01-17 19:32:13 +0300

    Пуш прилы

--- app/src/androidTest/java/Tests/MainScreenTests.kt
+++ app/src/androidTest/java/Tests/MainScreenTests.kt
@@ -26,7 +26,6 @@ class WelcomeTextTest : TestCase() {
                     }
                     goToFilesButton {
                         isDisplayed()
-                        click()
                     }
                 }
             }
